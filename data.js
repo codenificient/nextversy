dd33ab20322c296c8e465da85773d777db1e063e
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-07-14 17:05:16 -0400

    escape single quotes inside articales and redeploy

--- data.js
+++ data.js
@@ -8,23 +8,23 @@ export const articles = [
 	},
 	{
 		id: "2",
-		title: "What’s multi-cloud? And why should developers care?",
+		title: "What\’s multi-cloud? And why should developers care?",
 		excerpt:
-			"Most developers don’t care about multi-cloud. But they should...",
-		body: "Most developers don’t care about multi-cloud. But they should. Whether developers know it or not, their companies likely already have a multi-cloud environment.    Multi-cloud is a strategy where a business selects different services from different cloud providers",
+			"Most developers don\’t care about multi-cloud. But they should...",
+		body: "Most developers don\’t care about multi-cloud. But they should. Whether developers know it or not, their companies likely already have a multi-cloud environment.    Multi-cloud is a strategy where a business selects different services from different cloud providers",
 	},
 	{
 		id: "3",
 		title: "Here is how to make your website more accessible",
 		excerpt:
-			"An accessible website is one that’s optimized for all people, including those with disabilities...",
-		body: "There are many things to consider when setting up a website, and accessibility is one factor that can sometimes be overlooked. An accessible website is one that’s optimized for all people, including those with impaired vision or hearing, motor difficulties, or learning disabilities",
+			"An accessible website is one that\’s optimized for all people, including those with disabilities...",
+		body: "There are many things to consider when setting up a website, and accessibility is one factor that can sometimes be overlooked. An accessible website is one that\’s optimized for all people, including those with impaired vision or hearing, motor difficulties, or learning disabilities",
 	},
 	{
 		id: "4",
 		title: "Why open ecosystems are the future of app development",
 		excerpt:
 			"When app stores entered the mainstream tech culture, they exposed developers to an audience of millions...",
-		body: "We can’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities",
+		body: "We can'’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities",
 	},
 ]
