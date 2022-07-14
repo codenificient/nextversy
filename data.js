153f040bf88ae63bc06feab929b04de95242dcb3
CodenificienT <61930093+codenificient@users.noreply.github.com>
2022-07-14 17:07:57 -0400

    escape last work

--- data.js
+++ data.js
@@ -25,6 +25,6 @@ export const articles = [
 		title: "Why open ecosystems are the future of app development",
 		excerpt:
 			"When app stores entered the mainstream tech culture, they exposed developers to an audience of millions...",
-		body: "We can'’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities",
+		body: "We can\’t get enough of our mobile apps. There were 204 billion apps downloads last year, and that number is rising in 2020.  When app stores entered the mainstream tech culture, they exposed developers to an audience of millions who were keen to adopt the innovative capabilities",
 	},
 ]
